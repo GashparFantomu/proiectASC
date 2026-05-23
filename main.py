a572566646f93b5025e6162a61eedcd41b0162e9
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2026-05-23 16:56:03 +0300

    adaugat assembler pentru cod

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from models.asm_parser import ASMParser
 from models.instruction_loader import InstructionLoader
+from models.assembler import Assembler
 
 #interfata temporara
-print("--- Testare ASM Parser ---")
-parsed_asm = ASMParser.parse("test.asm")
-for line in parsed_asm:
-    print(line)
 
-print("\n--- Testare Instruction Loader ---")
-loader = InstructionLoader()
-opcodes = loader.load("Template_tema1.xlsx")
+def main():
+    print("\n--- Testare Instruction Loader ---")
+    loader = InstructionLoader()
+    opcodes = loader.load("Template_tema1.xlsx")
 
-for mnemonic, opcode_int in opcodes.items():
-    binary_str = format(opcode_int, '04b')
-    print(f"Instructiune: {mnemonic:<5} | Opcode (int): {opcode_int:<2} | Binar: {binary_str}")
+    print("--- Testare ASM Parser ---")
+    parsed_asm = ASMParser.parse("test.asm")
+
+    print("\n3. Generare Cod Masina (Asamblare):")
+    assembler = Assembler(opcodes)
+    hex_output = assembler.assemble(parsed_asm)
+
+
+    for original_tokens, hex_code in zip(parsed_asm, hex_output):
+        original_line = " ".join(original_tokens)
+        print(f"{original_line:<15} -> {hex_code}")
+if __name__ == "__main__":
+    main()
